132376cf24e3968f60071da4cfe28223bcd26484
vijeta33 <vijetahiwarkar@gmail.com>
2021-11-09 19:54:51 +0530

    API submission

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,38 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('Hello world.This is an updated response body. ')
+});
+
+router.get('/movies', function (req, res) {
+  res.send('[Jaibhim , Doctor, sooryavanshi]')
+});
+
+router.get('/movies/:movieIndex' ,function (req, res) {
+let movies = ["Jaibhim" , "Doctor", "sooryavanshi"]
+let value = req.params.movieIndex
+if (value > movies.length){
+    res.send("use a valid index")
+}else{
+    res.send(movies[value])
+}
+//let movieAtIndex = movies[index]
+
+});
+
+router.get('/films' , function (req, res){
+    let moviesobjects = [{"Id": 1 ,"name": "The Shining"},{"Id":2 , "name":"Incendies"},{"Id":3 , "name":"Finding Demo"}]
+    res.send(moviesobjects)
+});
+
+router.get('/films/:filmId', function (req, res) {
+  let moviesobjects = [{"Id": 1 ,"name": "The Shining"},{"Id":2 , "name":"Incendies"},{"Id":3 , "name":"Finding Demo"}]
+  let value = req.params.filmId 
+  if (value >= moviesobjects.length){
+      res.send("No such movie present")
+    }else{
+      res.send(moviesobjects[value])
+    }
 });
 
 module.exports = router;
